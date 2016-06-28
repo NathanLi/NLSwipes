4455c90919613b4b0f6bf6ee8f7cdd0efe3bc693
NathanLi <lixionglue@vip.qq.com>
2016-06-28 23:49:56 +0800

    Add FMDB

--- NLSwipes/AppDelegate.swift
+++ NLSwipes/AppDelegate.swift
@@ -14,16 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    print(NLTagModel.nl_propertyNames());
-    for name in NLTagModel.nl_propertyNames() {
-      let a = NLTagModel.nl_typeNameWithPropertyName(name)
-      print(a!)
-    }
-    
-    print(NLTagModel.db_tableName())
-    
-    let tagModel = NLTagModel()
-    print(tagModel.nl_toDictionary(true))
+
     return true
   }
 
---
+++ NLSwipes/Categories/DBHelper/NLDBHelperUtil.swift
@@ -0,0 +1,14 @@
+//
+//  NLDBHelperUtil.swift
+//  NLSwipes
+//
+//  Created by NathanLi on 16/3/10.
+//  Copyright © 2016年 NL. All rights reserved.
+//
+
+import Foundation
+
+struct NLDBHelperUtil {
+
+  
+}
